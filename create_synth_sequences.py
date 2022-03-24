7981b50998bda2474cb5c9dddecd81cff2811784
Erik Nikulski <erik@nikulski.me>
2022-03-24 11:04:00 +0100

    Adapt synthetic data creation to only contain unique sequences

--- create_synth_sequences.py
+++ create_synth_sequences.py
@@ -14,28 +14,28 @@ TEST_PATH = const.PROJECT_PATH + 'data/synthetic/test/'
 VALID_PATH = const.PROJECT_PATH + 'data/synthetic/valid/'
 
 
-def create_seq(alphabet, length):
-    doc = random.sample(alphabet, length)
-    code = doc
-    url = None
-    return {"docstring_tokens": doc, "code_tokens": code, "url": url}
-
-
 def create_set(alphabet, length, n):
-    return [create_seq(alphabet, length) for _ in range(n)]
+    samples = []
+    while len(samples) < n:
+        sample = random.sample(alphabet, length)
+        if sample not in samples:
+            samples.append(sample)
+    return [{"docstring_tokens": sample, "code_tokens": sample, "url": None} for sample in samples]
 
 
 def create():
+    samples = create_set(ALPHABET, LENGTH, N_TRAIN + N_TEST + N_VALID)
+
     with gzip.open(TRAIN_PATH + 'synth_train.jsonl.gz', 'wb') as f:
-        for item in create_set(ALPHABET, LENGTH, N_TRAIN):
+        for item in samples[:N_TRAIN]:
             f.write((json.dumps(item) + '\n').encode('utf-8'))
 
     with gzip.open(TEST_PATH + 'synth_test.jsonl.gz', 'wb') as f:
-        for item in create_set(ALPHABET, LENGTH, N_TEST):
+        for item in samples[N_TRAIN:N_TRAIN + N_TEST]:
             f.write((json.dumps(item) + '\n').encode('utf-8'))
 
     with gzip.open(VALID_PATH + 'synth_valid.jsonl.gz', 'wb') as f:
-        for item in create_set(ALPHABET, LENGTH, N_TEST):
+        for item in samples[N_TEST:]:
             f.write((json.dumps(item) + '\n').encode('utf-8'))
 
 
