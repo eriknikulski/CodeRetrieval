ccc12b16b00bb4805fbaf33c68bccfef67190112
Erik Nikulski <erik@nikulski.me>
2022-03-24 11:05:43 +0100

    Log test loss

--- train.py
+++ train.py
@@ -96,7 +96,7 @@ def train_loop(encoder, decoder, dataloader, loss_fn, encoder_optimizer, decoder
     return losses
 
 
-def test_loop(encoder, decoder, dataloader, loss_fn, max_length=const.MAX_LENGTH):
+def test_loop(encoder, decoder, dataloader, loss_fn, experiment, max_length=const.MAX_LENGTH):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     test_loss, correct = 0, 0
@@ -137,6 +137,7 @@ def test_loop(encoder, decoder, dataloader, loss_fn, max_length=const.MAX_LENGTH
             correct += (torch.cat(output).view(1, -1, current_batch_size).T == targets).all(axis=1).sum().item()
 
     test_loss /= num_batches
+    experiment.log_metric('test_batch_loss', test_loss)
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     return test_loss
@@ -162,7 +163,8 @@ def go_train(encoder, decoder, dataloader, test_dataloader, epochs=const.EPOCHS)
         for t in range(epochs):
             print(f"Epoch {t + 1}\n-------------------------------")
             losses_train.extend(train_loop(encoder, decoder, dataloader, loss_fn, encoder_optimizer, decoder_optimizer, experiment))
-            losses_test.append(test_loop(encoder, decoder, test_dataloader, loss_fn))
+            with experiment.test():
+                losses_test.append(test_loop(encoder, decoder, test_dataloader, loss_fn, experiment))
     showPlot(losses_train, 'train')
     showPlot(losses_test, 'test')
     print("Done!")
