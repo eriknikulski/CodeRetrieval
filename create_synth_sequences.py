557cee42341f4c964da53e63e76838ff26bbc433
Erik Nikulski <erik@nikulski.me>
2022-04-05 14:52:35 +0200

    Update synthetic data creation

--- create_synth_sequences.py
+++ create_synth_sequences.py
@@ -4,8 +4,9 @@ import random
 
 import const
 
-ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-LENGTH = 5
+BASE_ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+ALPHABET_LENGTH = 100
+SEQ_LENGTH = 5
 N_TRAIN = 4000
 N_TEST = 100000
 N_VALID = 1000
@@ -14,17 +15,25 @@ TEST_PATH = const.PROJECT_PATH + 'data/synthetic/test/'
 VALID_PATH = const.PROJECT_PATH + 'data/synthetic/valid/'
 
 
+def create_alphabet(base_alphabet, length):
+    alphabet = ['']
+    for i in range(length):
+        alphabet.append(alphabet[int(i / len(base_alphabet))] + base_alphabet[i % len(base_alphabet)])
+    return alphabet[1:]
+
+
 def create_set(alphabet, length, n):
     samples = []
     while len(samples) < n:
         sample = random.sample(alphabet, length)
         if sample not in samples:
             samples.append(sample)
-    return [{"docstring_tokens": sample, "code_tokens": sample, "url": None} for sample in samples]
+    return [{"docstring_tokens": sample, "code_tokens": random.sample(alphabet, length), "url": None} for sample in samples]
 
 
 def create():
-    samples = create_set(ALPHABET, LENGTH, N_TRAIN + N_TEST + N_VALID)
+    alphabet = create_alphabet(BASE_ALPHABET, ALPHABET_LENGTH)
+    samples = create_set(alphabet, SEQ_LENGTH, N_TRAIN + N_TEST + N_VALID)
 
     with gzip.open(TRAIN_PATH + 'synth_train.jsonl.gz', 'wb') as f:
         for item in samples[:N_TRAIN]:
