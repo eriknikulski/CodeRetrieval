e39fc0c21a97aa4cde2307b107e956bb39f6d9af
Erik Nikulski <erik@nikulski.me>
2022-06-09 16:09:28 +0200

    Set step in test data log

--- train.py
+++ train.py
@@ -140,8 +140,8 @@ def test_loop(encoder, decoder, dataloader, loss_fn, experiment, epoch_num):
     experiment.log_text(epoch_num + str(inputs) + ' ===> ' + str(result))
 
     test_loss /= num_batches
-    experiment.log_metric('test_batch_loss', test_loss)
-    experiment.log_metric('accuracy', 100*correct)
+    experiment.log_metric('test_batch_loss', test_loss, step=epoch_num)
+    experiment.log_metric('accuracy', 100*correct, step=epoch_num)
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     return test_loss
