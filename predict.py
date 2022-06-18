328885018b4437c5828a2ead02cd29ed62fced4e
Erik Nikulski <erik@nikulski.me>
2022-06-18 16:58:58 +0200

    Add prediction generation

--- predict.py
+++ predict.py
@@ -1,101 +1,84 @@
-from __future__ import unicode_literals, print_function, division
+import csv
+import pickle
 
-import numpy as np
 import pandas as pd
 import torch
+from subword_nmt import subword_nmt
 
 import const
 import data
 import model
-import loader
-
-# plt.switch_backend('agg')
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def evaluate(encoder, decoder, sentence, input_lang, output_lang, max_length=const.MAX_LENGTH):
+def evaluate(encoder, decoder, sentence, max_length=const.MAX_LENGTH):
     with torch.no_grad():
-        input_tensor = data.tensorFromSequence(input_lang, sentence)
-        input_length = input_tensor.size()[0]
         encoder_hidden = encoder.initHidden()
+        encoder_output, encoder_hidden = encoder(sentence, encoder_hidden)
 
-        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+        output = []
 
-        for ei in range(input_length):
-            encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
-            encoder_outputs[ei] += encoder_output[0, 0]
-
-        decoder_input = torch.tensor([[const.SOS_token]], device=device)  # SOS
-
-        decoder_hidden = encoder_hidden
-
-        decoded_words = []
+        decoder_input = torch.tensor([[const.SOS_TOKEN]], device=const.DEVICE)
+        decoder_hidden = torch.cat(tuple(el for el in encoder_hidden[0]), dim=1).view(1, 1, -1)
 
         for di in range(max_length):
             decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
             topv, topi = decoder_output.data.topk(1)
-            if topi.item() == const.EOS_token:
-                decoded_words.append('<EOS>')
-                break
-            else:
-                decoded_words.append(output_lang.index2word[topi.item()])
-
+            output.append(topi.item())
             decoder_input = topi.squeeze().detach()
-
-        return decoded_words
-
-
-def cosineSim(v1, v2):
-    return np.dot(v1, v2) / (np.norm(v1) * np.norm(v2))
+            if topi.item() == 1:
+                break
+        return output
 
 
-def bagSim(v1, v2):
-    if len(v2) > len(v1):
-        tmp = v2
-        v2 = v1
-        v1 = tmp
-    return sum(1 for word in v1 if word in v2) / len(v1)
+def preprocess(sentence, input_lang, bpe):
+    sentence = data.normalizeDocstring(sentence)
+    sentence = bpe.segment_tokens(sentence)
+    return input_lang.tensorFromSequence(sentence)
 
 
-def findMostSim(output_lang, words, pairs, func=bagSim):
-    words = words[:-1]
-    vec = data.tensorFromSequence(output_lang, words)
-    result = None
-    sMax = 0
-    for pair in pairs:
-        sim = func(vec, data.tensorFromSequence(output_lang, pair[1]))
-        if sim > sMax:
-            sMax = sim
-            result = pair
-
-    return result
+def match(seq, df):
+    res = df[df[['code_tokens']].applymap(lambda x: x == seq).values]
+    if len(res):
+        return res
+    return pd.DataFrame({'url': None}, index=[0])
 
 
 def run():
-    input_lang, output_lang, pairs = loader.get()
+    input_lang_file = open(const.INPUT_LANG_SAVE_PATH, 'rb')
+    input_lang = pickle.load(input_lang_file)
+    output_lang_file = open(const.OUTPUT_LANG_SAVE_PATH, 'rb')
+    output_lang = pickle.load(output_lang_file)
 
-    encoder = model.EncoderRNN(input_lang.n_words, const.HIDDEN_SIZE).to(device)
-    encoder.load_state_dict(torch.load(const.SAVE_PATH + 'encoder.pt'))
+    encoder = model.EncoderRNN(input_lang.n_words, const.HIDDEN_SIZE, 1, input_lang).to(const.DEVICE)
+    encoder.load_state_dict(torch.load(const.ENCODER_PATH))
     encoder.eval()
 
-    decoder = model.DecoderRNN(const.HIDDEN_SIZE, output_lang.n_words).to(device)
-    decoder.load_state_dict(torch.load(const.SAVE_PATH + 'decoder.pt'))
+    decoder = model.DecoderRNN(const.BIDIRECTIONAL * const.ENCODER_LAYERS * const.HIDDEN_SIZE,
+                               output_lang.n_words, 1, output_lang).to(const.DEVICE)
+    decoder.load_state_dict(torch.load(const.DECODER_PATH))
     decoder.eval()
 
     queries = pd.read_csv(const.QUERY_CSV_PATH)
+    all_data = pd.read_pickle(const.ALL_DATA_DF_SAVE_PATH)
+    all_data[['code_tokens']] = all_data[['code_tokens']].applymap(lambda x: list(x.flatten()))
+
+    with open(const.PREPROCESS_CODES_PATH, encoding='utf-8') as codes_file:
+        bpe = subword_nmt.BPE(codes_file)
+
+    predictions = []
 
     for i, row in queries.iterrows():
-        print()
-        print()
-        print(row['query'])
-        output_words = evaluate(encoder, decoder, row['query'].split(), input_lang, output_lang)
-        found = [pair for pair in pairs if output_words == pairs[1]]
-        sim = findMostSim(output_lang, output_words, pairs)
-        print(output_words)
-        print(found)
-        print(sim)
-        if i > 5:
-            break
+        sentence = preprocess(row['query'].split(), input_lang, bpe)
+        output_seq = evaluate(encoder, decoder, sentence)
+        output_sentence = ' '.join(decoder.lang.seqFromIndices(output_seq))
+        output = match(output_seq, all_data)
+
+        predictions.append(['Java', row['query'], output.at[0, 'url']])
+
+    with open(const.MODEL_PREDICTIONS_CSV, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(['language', 'query', 'url'])
+        writer.writerows(predictions)
 
 
 if __name__ == '__main__':
