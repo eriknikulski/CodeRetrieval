eb5ce82aec0bffcaa7f1501df09a8c3eb6e0490e
Erik Nikulski <erik@nikulski.me>
2022-06-18 12:11:24 +0200

    Log accuracy after calculation

--- train.py
+++ train.py
@@ -146,9 +146,9 @@ def test_loop(encoder, decoder, dataloader, loss_fn, experiment, epoch_num):
                         '\n'.join(str(input) + '  ====>  ' + str(result) for input, result in zip(inputs, results)))
 
     test_loss /= num_batches
+    correct /= size
     experiment.log_metric('test_batch_loss', test_loss, step=epoch_num)
     experiment.log_metric('accuracy', 100*correct, step=epoch_num)
-    correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     return test_loss
 
