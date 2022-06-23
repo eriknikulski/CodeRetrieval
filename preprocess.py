cd9cf8653cb2a61969092f40fff439fb4e1adc72
Erik Nikulski <erik@nikulski.me>
2022-06-23 13:02:00 +0200

    Add byte pair encoding to code tokens

--- preprocess.py
+++ preprocess.py
@@ -35,26 +35,44 @@ def run(args):
     valid_data = loader.CodeDataset(const.PROJECT_PATH + data_path + 'valid/', labels_only=const.LABELS_ONLY,
                                     build_language=False, remove_duplicates=remove_duplicates, to_tensors=False)
 
-    print('Creating training file...')
-    with open(const.PREPROCESS_TRAIN_PATH, 'w', encoding='utf-8') as train_file:
+    print('Creating training files...')
+    with open(const.PREPROCESS_TRAIN_PATH_DOC, 'w', encoding='utf-8') as train_file:
         for text in train_data.df['docstring_tokens']:
             train_file.write(f'{" ".join(text)}\n')
         for text in test_data.df['docstring_tokens']:
             train_file.write(f'{" ".join(text)}\n')
         for text in valid_data.df['docstring_tokens']:
             train_file.write(f'{" ".join(text)}\n')
+    if not const.LABELS_ONLY:
+        with open(const.PREPROCESS_TRAIN_PATH_CODE, 'w', encoding='utf-8') as train_file:
+            for text in train_data.df['code_tokens']:
+                train_file.write(f'{" ".join(text)}\n')
+            for text in test_data.df['code_tokens']:
+                train_file.write(f'{" ".join(text)}\n')
+            for text in valid_data.df['code_tokens']:
+                train_file.write(f'{" ".join(text)}\n')
 
-    print('Creating codes file...')
-    with open(const.PREPROCESS_TRAIN_PATH, encoding='utf-8') as train_file, \
-            open(const.PREPROCESS_CODES_PATH, 'w', encoding='utf-8') as codes_file:
-        subword_nmt.learn_bpe(train_file, codes_file, const.PREPROCESS_VOCAB_SIZE)
+    print('Creating codes files...')
+    with open(const.PREPROCESS_TRAIN_PATH_DOC, encoding='utf-8') as train_file, \
+            open(const.PREPROCESS_CODES_PATH_DOC, 'w', encoding='utf-8') as codes_file:
+        subword_nmt.learn_bpe(train_file, codes_file, const.PREPROCESS_VOCAB_SIZE_DOC)
+    if not const.LABELS_ONLY:
+        with open(const.PREPROCESS_TRAIN_PATH_CODE, encoding='utf-8') as train_file, \
+                open(const.PREPROCESS_CODES_PATH_CODE, 'w', encoding='utf-8') as codes_file:
+            subword_nmt.learn_bpe(train_file, codes_file, const.PREPROCESS_VOCAB_SIZE_CODE)
 
     print('Applying codes...')
-    with open(const.PREPROCESS_CODES_PATH, encoding='utf-8') as codes_file:
+    with open(const.PREPROCESS_CODES_PATH_DOC, encoding='utf-8') as codes_file:
         bpe = subword_nmt.BPE(codes_file)
         train_data.df[['docstring_tokens']] = train_data.df[['docstring_tokens']].applymap(bpe.segment_tokens)
         test_data.df[['docstring_tokens']] = test_data.df[['docstring_tokens']].applymap(bpe.segment_tokens)
         valid_data.df[['docstring_tokens']] = valid_data.df[['docstring_tokens']].applymap(bpe.segment_tokens)
+    if not const.LABELS_ONLY:
+        with open(const.PREPROCESS_CODES_PATH_CODE, encoding='utf-8') as codes_file:
+            bpe = subword_nmt.BPE(codes_file)
+            train_data.df[['code_tokens']] = train_data.df[['code_tokens']].applymap(bpe.segment_tokens)
+            test_data.df[['code_tokens']] = test_data.df[['code_tokens']].applymap(bpe.segment_tokens)
+            valid_data.df[['code_tokens']] = valid_data.df[['code_tokens']].applymap(bpe.segment_tokens)
 
     print('Working on dataframe...')
     if const.LABELS_ONLY:
