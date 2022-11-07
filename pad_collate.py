c065b959f8fd577d2c040b361b3924d9c8e49782
Erik Nikulski <erik@nikulski.me>
2022-11-07 17:55:53 +0100

    Access device tensor via property

--- pad_collate.py
+++ pad_collate.py
@@ -15,7 +15,7 @@ def pad_tensor(vec, pad, dim):
     """
     pad_size = list(vec.shape)
     pad_size[dim] = pad - vec.size(dim)
-    device = vec.get_device() if torch.cuda.is_available() else None
+    device = vec.device if torch.cuda.is_available() else None
     return torch.cat([vec, torch.full(pad_size, const.PAD_TOKEN, dtype=torch.long).to(device)], dim=dim)
 
 
