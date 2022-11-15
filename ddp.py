a7f650f24f7dc4bdc1430b7dd24cdd77c2282056
Erik Nikulski <erik@nikulski.me>
2022-11-15 16:40:11 +0100

    Add utility methods on ddp

--- ddp.py
+++ ddp.py
@@ -35,3 +35,34 @@ def cleanup():
 def run(fn, world_size, experiment_name, port):
     mp.spawn(fn, args=(world_size, experiment_name, port,),
              nprocs=world_size, join=True)
+
+
+def is_dist_avail_and_initialized():
+    if not dist.is_available():
+        return False
+
+    if not dist.is_initialized():
+        return False
+
+    return True
+
+
+def save_on_master(*args, **kwargs):
+    if is_main_process():
+        torch.save(*args, **kwargs)
+
+
+def get_rank():
+    if not is_dist_avail_and_initialized():
+        return 0
+
+    return dist.get_rank()
+
+
+def is_main_process():
+    return get_rank() == 0
+
+
+def sync():
+    if is_dist_avail_and_initialized():
+        dist.barrier()
