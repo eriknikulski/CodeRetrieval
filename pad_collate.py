255bc31086e9353be67f03670ad629231c814d05
Erik Nikulski <erik@nikulski.me>
2022-12-19 15:44:58 +0100

    Pad with padding token as opposed to zero

--- pad_collate.py
+++ pad_collate.py
@@ -1,7 +1,10 @@
 import numpy as np
 from torch.nn.utils.rnn import pad_sequence
 
+import const
+
 
 def collate(batch):
     batch = np.array(batch)
-    return pad_sequence(batch[::, 0], batch_first=True), pad_sequence(batch[::, 1], batch_first=True), batch[::, 2]
+    return pad_sequence(batch[::, 0], batch_first=True, padding_value=const.PAD_TOKEN), \
+        pad_sequence(batch[::, 1], batch_first=True, padding_value=const.PAD_TOKEN), batch[::, 2]
